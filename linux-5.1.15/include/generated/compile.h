c29d78a1b8a35690c76a158584c9fef2377fdc41
aireat <aireat@gmail.com>
2019-06-27 16:36:04 +0900

    [06주차.추가] 컴파일시 생성되는 include files 추가

---
+++ linux-5.1.15/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Jun 27 15:00:19 KST 2019"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "linux"
+#define LINUX_COMPILER "gcc version 7.4.0 (Ubuntu/Linaro 7.4.0-1ubuntu1~18.04.1)"
